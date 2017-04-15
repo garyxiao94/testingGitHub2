25b709f05657a886866ebcdb56589aa4b6094e9e
Gary Xiao <gary.xiao94@gmail.com>
2017-04-16 01:10:55 +1000

    Initial Commit

---
+++ TestingGitHub2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingGitHub2
+//
+//  Created by Gary Xiao on 16/4/17.
+//  Copyright © 2017 Gary Xiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
